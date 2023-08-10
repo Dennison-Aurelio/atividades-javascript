a62b02555f3c4b6a73a0f8769d95879906a8aa08
Dennison Aurelio <111392740+Dennison-Aurelio@users.noreply.github.com>
2023-08-09 22:39:33 -0300

    Add files via upload

---
+++ aula19/pratica-1/pratica1.js
@@ -0,0 +1,13 @@
+const nome = 'Dennison'
+let numero1 = 6
+let numero2 = 5
+
+const soma = numero1 + numero2;
+let multiplicacao = numero1 * numero2;
+
+alert(soma)
+
+console.log(nome)
+console.log(multiplicacao)
+
+
